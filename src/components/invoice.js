09c52892233ea845bcaef797f0f0cd6841b48022
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-01-18 14:48:06 +0100

    Added background image, started adding style s

--- src/components/invoice.js
+++ src/components/invoice.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import Table from '../UI/Table';
+import Grid from '@material-ui/core/Grid';
+/* TODO: Look up CSSBASELINE! */
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		height: '100vh',
+	},
+	image: {
+		backgroundImage:
+			'url(https://res.cloudinary.com/whatwherewhen/image/upload/v1610977367/xn9y3a5t7wxwcty2vfoe.png)',
+		backgroundRepeat: 'no-repeat',
+		backgroundColor: theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+	},
+	paper: {
+		margin: theme.spacing(4, 8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+}));
 
 const Invoice = () => {
+	const classes = useStyles();
 	return (
-		<div>
-			<Table />
-		</div>
+		<Grid container component="main" className={classes.root}>
+			<CssBaseline />
+			<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+				<Table />
+			</Grid>
+			<Grid item xs={false} sm={4} md={7} className={classes.image} />
+		</Grid>
 	);
 };
 
