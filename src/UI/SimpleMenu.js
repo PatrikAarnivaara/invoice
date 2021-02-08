18baae2e283cc5fd59770b9246c2d2223049d3ee
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-02-08 15:29:44 +0100

    Edited menu parts

--- src/UI/SimpleMenu.js
+++ src/UI/SimpleMenu.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button, Menu, MenuItem } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(() => ({
@@ -9,31 +9,16 @@ const useStyles = makeStyles(() => ({
 		position: 'absolute',
 		right: '2em',
 		top: '3em',
-	}
+	},
 }));
 
 const SimpleMenu = () => {
 	const classes = useStyles();
-	const [anchorEl, setAnchorEl] = useState(null);
-
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
-
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
-
 	return (
 		<div className={classes.root}>
-			<Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+			<Button>
 				<MenuIcon />
 			</Button>
-			<Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-				<MenuItem onClick={handleClose}>Profile</MenuItem>
-				<MenuItem onClick={handleClose}>My account</MenuItem>
-				<MenuItem onClick={handleClose}>Logout</MenuItem>
-			</Menu>
 		</div>
 	);
 };
